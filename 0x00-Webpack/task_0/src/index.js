9d09dfb1f03d89c975e7a6537e541db43c109afb
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-07-25 07:55:53 +0100

    Basic setup

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,16 +1,5 @@
-import $ from 'jquery';
+import $  from "jquery";
 
-
-function addParagraphs() {
-  const body = $('body');
-
-  const paragraph1 = $('<p>').text('Holberton Dashboard');
-  const paragraph2 = $('<p>').text('Dashboard data for the students');
-  const paragraph3 = $('<p>').text('Copyright - Holberton School');
-
-  body.append(paragraph1, paragraph2, paragraph3);
-}
-
-$(document).ready(function () {
-  addParagraphs();
-});
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
